93569a3ba5af6ff6a28afdfc49f936f8b8188f2a
Zorin Arseniy <Arsenchozzz@mail.ru>
2019-06-17 19:05:02 +0400

    Добавьте файлы проекта.

---
+++ DataWorks_Test/DoublyLinkedList.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataWorks_Test
+{
+    class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        DoublyNode<T> head; // головной/первый элемент
+        DoublyNode<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        // добавление элемента
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+        // удаление
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            // поиск удаляемого узла
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                // если узел не последний
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    // если последний, переустанавливаем tail
+                    tail = current.Previous;
+                }
+
+                // если узел не первый
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    // если первый, переустанавливаем head
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reverse()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                var buffer = tail.Data;
+                Remove(tail.Data);
+                AddFirst(buffer);
+            }
+        }
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            DoublyNode<T> current = tail;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Previous;
+            }
+        }
+    }
+}
---
+++ DataWorks_Test/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataWorks_Test
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            abc();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int s = RomanToInt(textBox.Text);
+            if (s != 0)
+            {
+                label.Content = s;
+            }
+            else
+            {
+                label.Content = "Неверный ввод";
+            }
+        }
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool b = CheckBrackets(textBox1.Text);
+            if (b)
+            {
+                label1.Content="Скобочная структура сбалансирована";
+            }
+            else
+            {
+                label1.Content = "Скобочная структура НЕ сбалансирована";
+            }
+        }
+
+        public static int RomanToInt(string text)
+        {
+            Dictionary<char, int> numbers;
+            numbers = new Dictionary<char, int>
+            {
+                { 'I', 1},
+                { 'V', 5},
+                { 'X', 10},
+                { 'L', 50},
+                { 'C', 100},
+                { 'D', 500},
+                { 'M', 1000}
+            };
+            int result = 0, prevValue = 0;
+            foreach (char chr in text)
+            {
+                // если такого символа нету 
+                if (!numbers.ContainsKey(chr))
+                {
+                    // выход из метода 
+                    return 0;
+                }
+                // получаем текущее значение из словарика 
+                int curValue = numbers[chr];
+                // складываем в результат 
+                result += curValue;
+                // если предыдущее значение не равно 0 И меньше текущего 
+                if (prevValue != 0 && prevValue < curValue)
+                {
+                    // если предыдущее значение это 1, а текущее 5 или 10 , ИЛИ 
+                    // 10 с 50 или 100 
+                    // 100 с 500 или 1000 
+                    if (prevValue == 1 &&
+                    (curValue == 5 || curValue == 10) ||
+                    prevValue == 10 &&
+                    (curValue == 50 || curValue == 100) ||
+                    prevValue == 100 &&
+                    (curValue == 500 || curValue == 1000))
+                    {
+                        // результат уменьшить на 
+                        result -= 2 * prevValue;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                // предыдущему значению даём текущее 
+                prevValue = curValue;
+            }
+            return result;
+        }
+        public static bool CheckBrackets(string text)
+        {
+            bool answer = false;
+            int rang = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(')
+                {
+                    rang++;
+                }
+                if (text[i] == ')')
+                {
+                    rang--;
+                }
+            }
+            if (rang == 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            return answer;
+        }
+
+        public static void abc()
+        {
+            DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            // добавление элементов
+            linkedList.Add("Bob");
+            linkedList.Add("Bill");
+            linkedList.Add("Tom");
+            linkedList.AddFirst("Kate");
+
+            MessageBox.Show("прямой порядок");
+            foreach (var item in linkedList)
+            {
+                MessageBox.Show(item);
+            }
+            // удаление
+            linkedList.Remove("Bill");
+
+            MessageBox.Show("прямой порядок после удаления элемента ");
+            foreach (var item in linkedList)
+            {
+                MessageBox.Show(item);
+            }
+
+            MessageBox.Show("обратный порядок");
+            foreach (var t in linkedList.BackEnumerator())
+            {
+                MessageBox.Show(t);
+            }
+            linkedList.Reverse();
+            foreach (var item in linkedList)
+            {
+                MessageBox.Show(item);
+            } 
+        }
+    }
+}
